ef3e8754c6927d4dcd5e370578da0b77686f00c0
CTXz <ctx.xda@gmail.com>
2016-03-16 16:27:43 +0100

    suzuran: Fix minor porting mistake
    Change-Id: Icf43bc3979511dab9eac0fc958847d029fc7cf94

--- cm.mk
+++ cm.mk
@@ -4,6 +4,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-$(call inherit-product, device/sony/kminilte/full_kminilte.mk)
+$(call inherit-product, device/sony/suzuran/full_suzuran.mk)
 
 PRODUCT_NAME := cm_suzuran
