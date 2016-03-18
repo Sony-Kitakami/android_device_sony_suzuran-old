fee2ac1697eeadade415aad350b9f40bbf0dc75c
CTXz <ctx.xda@gmail.com>
2016-03-18 22:33:58 +0100

    suzuran: Change CM specific paths to Omni

--- omni_suzuran.mk
+++ omni_suzuran.mk
@@ -1,8 +1,8 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/omni/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/suzuran/full_suzuran.mk)
 
