786e5100502ad800f19e70a5a4ca574d5d8857f4
CTXz <ctx.xda@gmail.com>
2016-03-18 22:37:35 +0100

    suzuran: Remove non-existing paths

--- omni_suzuran.mk
+++ omni_suzuran.mk
@@ -1,9 +1,3 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/omni/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/omni/config/nfc_enhanced.mk)
-
 $(call inherit-product, device/sony/suzuran/full_suzuran.mk)
 
 PRODUCT_NAME := omni_suzuran
